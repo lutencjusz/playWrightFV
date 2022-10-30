a3d679935f2060e4626eb1a4c5cf0c3020802f8b
lutencjusz <lutencjusz@gmail.com>
2022-10-30 08:42:17 +0100

    reorganizacja plików

---
+++ src/test/java/org/example/utils/ConsoleColors.java
@@ -0,0 +1,8 @@
+package org.example.utils;
+
+public class ConsoleColors {
+    // Reset
+    public static final String RESET = "\033[0m";  // Text Reset
+    public static final String YELLOW = "\033[0;33m";  // YELLOW
+    public static final String RED_BOLD = "\033[1;31m";    // RED
+}
